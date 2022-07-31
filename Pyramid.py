b4dbf9716fabd72f7a3c2c54757108b0034c30bf
Northy24 <thaithat788@gmail.com>
2022-07-31 17:13:43 +0700

    edit pyramid devlab

--- Pyramid.py
+++ Pyramid.py
@@ -1,16 +1,17 @@
 row = int(input())
 col = (2*row) - 1
 
-n = 0
-m = 0
+n = 1
+m = 1
 
-while n < 3 :
+while n <= 3 :
     while m <= col :
-        if m >= row - n and m <= row + n  :
+        if m >= row - (n-1) and m <= row + (n-1)  :
             print("*", end = "")
         else :
             print(" ", end ="")
         m += 1
     n += 1
     print("")
-    m = 0
+    m = 1
+    
